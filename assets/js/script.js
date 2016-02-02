7c77f13ffbb4136b0b38912797e7a988a81a5db7
marktorrano <mark.torrano@44778-501.auckland.natcoll.ac.nz>
2016-02-03 09:43:56 +1300

    Added hiding and opening accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,68 @@
 //$(document).ready(function(){
 //
-//    $('#slide-button').on('click', function(){
-//        $('header').toggleClass('headeractive turnblue');        
-//        if($('#slide-button>i').hasClass('fa-arrow-circle-right')){
-//            $('#slide-button>i').removeClass('fa-arrow-circle-right');
-//            $('#slide-button>i').addClass('fa-arrow-circle-left');            
-//        }else{
-//            $('#slide-button>i').removeClass('fa-arrow-circle-left');
-//            $('#slide-button>i').addClass('fa-arrow-circle-right');            
-//        }
-//        trans('header', .5);
-//    });
 //    
-//    function trans(elem, sec) {
-//        $(elem).css('-webkit-transition' , 'all ' +sec+ 's ease');
-//        $(elem).css('-moz-transition' , 'all ' +sec+ 's ease');
-//        $(elem).css('-ms-transition' , 'all ' +sec+ 's ease');
-//        $(elem).css('-o-transition' , 'all ' +sec+ 's ease');
-//        $(elem).css('transition' , 'all ' +sec+ 's ease');
-//    }
 //    
 //});
 
 $(document).ready(function(){
     
     var isOpen = false;
+    var aIsOpen = false;
 
+//    $('#slide-button').on('click', function(){
+//
+//        if(!isOpen){
+//            $('header').animate({left: '0em'}, 500, function(){
+//                $('#slide-button>i').removeClass('fa-arrow-circle-right').addClass('fa-arrow-circle-left'); 
+//                $('header').addClass('turnblue');
+//            }); 
+//            isOpen = true;
+//        }else{
+//            $('header').animate({left: '-17em'}, 500, function(){
+//                $('#slide-button>i').removeClass('fa-arrow-circle-left').addClass('fa-arrow-circle-right'); 
+//                $('header').removeClass('turnblue');
+//            }); 
+//            isOpen = false;
+//        }
+//
+//    }); 
     $('#slide-button').on('click', function(){
-
-        if(!isOpen){
-            $('header').animate({left: '0em'}, 500, function(){
-                $('#slide-button>i').removeClass('fa-arrow-circle-right').addClass('fa-arrow-circle-left'); 
-                $('header').addClass('turnblue');
-            }); 
-            isOpen = true;
+        $('header').toggleClass('headeractive turnblue');        
+        if($('#slide-button>i').hasClass('fa-arrow-circle-right')){
+            $('#slide-button>i').removeClass('fa-arrow-circle-right');
+            $('#slide-button>i').addClass('fa-arrow-circle-left');            
         }else{
-            $('header').animate({left: '-17em'}, 500, function(){
-                $('#slide-button>i').removeClass('fa-arrow-circle-left').addClass('fa-arrow-circle-right'); 
-                $('header').removeClass('turnblue');
-            }); 
-            isOpen = false;
+            $('#slide-button>i').removeClass('fa-arrow-circle-left');
+            $('#slide-button>i').addClass('fa-arrow-circle-right');            
         }
+        trans('header', .5);
+    });
+    
+    function trans(elem, sec) {
+        $(elem).css('-webkit-transition' , 'all ' +sec+ 's ease');
+        $(elem).css('-moz-transition' , 'all ' +sec+ 's ease');
+        $(elem).css('-ms-transition' , 'all ' +sec+ 's ease');
+        $(elem).css('-o-transition' , 'all ' +sec+ 's ease');
+        $(elem).css('transition' , 'all ' +sec+ 's ease');
+    }
+    
+    $('#accordion p').hide();
+    $('#accordion li h2').on('click',function(){
 
-    }); 
+        if(!aIsOpen){
+            $(this.nextElementSibling).slideDown(function(){
+                $(this.previousElementSibling.firstChild).removeClass('fa-chevron-circle-down');
+                $(this.previousElementSibling.firstChild).addClass('fa-chevron-circle-up');
+            });
+            aIsOpen = true;
+        }else{
+           $(this.nextElementSibling).slideUp(function(){
+                $(this.previousElementSibling.firstChild).removeClass('fa-chevron-circle-up');
+                $(this.previousElementSibling.firstChild).addClass('fa-chevron-circle-down');
+            }); 
+            aIsOpen = false;
+        }
+    });
+
     
 });
